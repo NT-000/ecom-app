5c476e613af1fdbd637dbb0146a9d496efcdaa7c
NT-000 <nicolai.thue.reier@gmail.com>
2025-06-30 12:34:21 +0200

    fix

--- src/routes/api/purchase-confirmation/+server.js
+++ src/routes/api/purchase-confirmation/+server.js
@@ -2,7 +2,7 @@ import { json } from '@sveltejs/kit';
 
 export async function POST({ request }) {
 	const req = await request.json();
-	console.log('request body:', req);
+	console.log('request body from pc:', req);
 
 	return json({ success: true });
 }
