b4df4e3964ce1c8826d981eabe1c311682d9e3be
NT-000 <nicolai.thue.reier@gmail.com>
2025-06-30 09:55:41 +0200

    ...

--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,11 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 export default {
 	preprocess: vitePreprocess(),
 	kit: {
-		prerender: {
-			entries: ['*']
-		},
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: 'index.html',
-			strict: true
-		}),
+		adapter: adapter(),
 		alias: {
 			$components: 'src/lib/components',
 			$assets: 'src/assets'
