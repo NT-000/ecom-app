8b065b5896f9f81669fc238360aa1af3000dfadd
NT-000 <nicolai.thue.reier@gmail.com>
2025-07-08 09:46:03 +0200

    Add checkout success and failure routes

---
+++ src/routes/checkout/failure/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+	import { Button, HeroSection } from '$components';
+</script>
+
+<HeroSection>
+	<h1>ERROR WHILE</h1>
+	<h1 class="mb-l">PURCHASING</h1>
+	<p class="mb-s">
+		It seems like something went wrong when trying to purchasing the book.
+		Please check that your card credentials are up to date.
+	</p>
+	<p class="mb-s">
+		Dr. Dean apologizes for any inconvenience and invites you to try again.
+	</p>
+	<p>
+		If you like to try again, please click the button below.
+	</p>
+	<Button class="gradient-gold mt-m">Try again</Button>
+</HeroSection>
